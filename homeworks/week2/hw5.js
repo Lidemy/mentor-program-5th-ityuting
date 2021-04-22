a382082edc61104446317599cdd535789e7e85a4
ityuting <yuting940256@gmail.com>
2021-04-22 23:25:18 +0800

    final week2 hw version

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,20 @@
 function join(arr, concatStr) {
-  
+    var str = "";
+    for(var i = 0; i<arr.length; i++){
+        str = str + arr[i];
+        if(i < arr.length-1){
+            str = str +concatStr;
+        }
+    }
+    return str;
 }
 
 function repeat(str, times) {
-  
+    var newstr = "";
+    for(var i = 1; i <= times; i++){
+        newstr += str;
+    }
+    return newstr;
 }
 
 console.log(join(['a'], '!'));
