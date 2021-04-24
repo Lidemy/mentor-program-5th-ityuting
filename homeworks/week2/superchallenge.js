3bd18446efefdfc3ce005ea2bba16da8e304b6ae
ityuting <81981640+ityuting@users.noreply.github.com>
2021-04-24 16:13:00 +0800

    Update superchallenge.js
    這是更改過的～但因為是用位元運算，數值太大會太小可能會 overflow or underflow，造成結果錯誤。

--- homeworks/week2/superchallenge.js
+++ homeworks/week2/superchallenge.js
@@ -1,3 +1,21 @@
-function add(a, b) {
-    return ((((a & b) << 1) ^ (a ^ b)) | ((((a & b) << 1) & (a ^ b)) << 1))
+function add(a,b){
+    a_length = String(a.toString("2")).length;
+    b_length = String(b.toString("2")).length;
+    let max = 0;
+    if (a_length >= b_length){
+        max = a_length;
+    } else {
+        max = b_length;
+    }
+    let carry = (a & b) << 1;
+    let plus = a ^ b;
+    let newcarry = 0;
+    let newplus = 0;
+    for (let loops = 2; loops <= max; loops++){
+        newcarry = (carry & plus) << 1;
+        newplus = (carry ^ plus);
+        carry = newcarry;
+        plus = newplus;
+    }
+    return carry|plus;
 }
